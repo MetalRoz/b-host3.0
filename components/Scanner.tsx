0a7a777211ccdb39c6b6a21c55b883bcaa69b53f
Wilson <wilsonmiguel2003@gmail.com>
2024-07-08 12:59:01 -0400

    test

--- components/Scanner.tsx
+++ components/Scanner.tsx
@@ -1,42 +1,8 @@
-import { CameraView, useCameraPermissions } from "expo-camera";
 import React from "react";
-import { Alert, Button, StyleSheet, View } from "react-native";
+
 
 const Scanner = () => {
-  const handleScanned = (data: any) => {
-    Alert.alert(data);
-  };
-  const [permission, setPermission] = useCameraPermissions();
-
-  const tenerpermiso = () => {
-    setPermission();
-
-    if (!permission?.granted) {
-      return null;
-    }
-
-    return (
-      <View>
-        <CameraView
-          barcodeScannerSettings={{ barcodeTypes: ["qr"] }}
-          onBarcodeScanned={handleScanned}
-        />
-      </View>
-    );
-  };
-
-  return (
-    <View style={styles.view}>
-        <Button title="PRESIONAME" onPress={tenerpermiso}></Button>
-    </View>
-  )
+ return null
 };
-
-const styles = StyleSheet.create({
-    view: {
-        padding: 10,
-        margin: 40
-    }
-})
 
 export default Scanner;
