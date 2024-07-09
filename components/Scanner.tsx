913fffca3dd30fca9b9ca8ac9e3aff5c6f101d1e
Wilson <wilsonmiguel2003@gmail.com>
2024-07-09 08:46:24 -0400

    test

--- components/Scanner.tsx
+++ components/Scanner.tsx
@@ -1,25 +1,44 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, Button, StyleSheet } from "react-native";
-import { RNCamera } from "react-native-camera";
+import { Camera } from "expo-camera";
+import { BarCodeScanner } from "expo-barcode-scanner";
 import Modal from "react-native-modal";
 
 const Scanner = () => {
+  const [hasPermission, setHasPermission] = useState<null | boolean>(null);
   const [isCameraVisible, setIsCameraVisible] = useState(false);
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [qrData, setQrData] = useState(null);
 
-  const handleBarCodeRead = ({ data }: any) => {
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }: any) => {
     setQrData(data);
     setIsCameraVisible(false);
     setIsModalVisible(true);
   };
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
 
   return (
     <View style={styles.container}>
       <Button title="Scan QR Code" onPress={() => setIsCameraVisible(true)} />
 
       {isCameraVisible && (
-        <RNCamera style={styles.camera} onBarCodeRead={handleBarCodeRead} />
+        <BarCodeScanner
+          onBarCodeScanned={handleBarCodeScanned}
+          style={StyleSheet.absoluteFillObject}
+        />
       )}
 
       <Modal isVisible={isModalVisible}>
